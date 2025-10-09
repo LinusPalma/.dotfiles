24a9e9a69a6539d69f1e53a3f44a1207ee7d718d
lpalma <linus.palma@gmail.com>
2025-10-09 17:37:30 +0200

    feat(nvim): add line numbers by default (-> relative) and fixed an issue with autosave on write

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,6 @@
 require("config.vim")
 require("config.lazy")
+
+-- wenn nach branch wechsel Lazy error in der GUI für die checks anzeigt dann die packages einmal löschen:
+-- rm -rf ~/.local/share/nvim/lazy/mason-lspconfig.nvim
+-- rm -rf ~/.local/share/nvim/lazy/mason.nvim
--- nvim/lua/config/vim.lua
+++ nvim/lua/config/vim.lua
@@ -6,17 +6,10 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set clipboard=unnamedplus")
 
 -- für copy & paste mit clipboard wenn nvim über ssh läuft:
-vim.g.clipboard = 'osc52'
-vim.o.winborder = 'rounded'
-
+vim.g.clipboard = "osc52"
+vim.o.winborder = "rounded"
 
 vim.opt.autowrite = true
 vim.opt.autowriteall = true
 
-
--- Format on Save
-vim.api.nvim_create_autocmd("BufWritePre", {
-  callback = function()
-    vim.lsp.buf.format({ timeout_ms = 2000 })
-  end,
-})
+vim.opt.relativenumber = true
