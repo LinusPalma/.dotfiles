e642f69eca5966ffd3e823bc3b0d5dcdcc06e0cc
lpalma <linus.palma@gmail.com>
2025-10-19 12:43:47 +0200

    feat(nvim): add gruv theme to nvim

--- roles/nvim/files/lua/plugins/colorscheme.lua
+++ roles/nvim/files/lua/plugins/colorscheme.lua
@@ -1,15 +1,26 @@
 return {
-  -- the colorscheme should be available when starting Neovim
-  {
-    "folke/tokyonight.nvim",
-    -- "catppuccin/nvim",
-    -- name = "catppuccin",
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-      -- vim.cmd.colorscheme "catppuccin"
-    end,
-  }
+	-- the colorscheme should be available when starting Neovim
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			--		vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme gruvbox]])
+		end,
+	},
+	{
+
+		-- "catppuccin/nvim",
+		-- name = "catppuccin",
+		-- vim.cmd.colorscheme "catppuccin"
+	},
 }
