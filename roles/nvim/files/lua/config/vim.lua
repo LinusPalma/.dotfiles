9466256e2dcd089b90b4c200ab6f3fbfe8fcb172
lpalma <linus.palma@gmail.com>
2025-10-19 08:49:22 +0200

    feat(dotfiles): added shortcuts to move lines via alt. added highlight for current line

--- roles/nvim/files/lua/config/vim.lua
+++ roles/nvim/files/lua/config/vim.lua
@@ -1,9 +1,8 @@
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-
 vim.cmd("set clipboard=unnamedplus")
+vim.cmd("set softtabstop=2")
 
 -- für copy & paste mit clipboard wenn nvim über ssh läuft:
 vim.g.clipboard = "osc52"
@@ -13,3 +12,12 @@ vim.opt.autowrite = true
 vim.opt.autowriteall = true
 
 vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.cursorline = true
+-- vim.opt.cursorlineopt = "number"
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = " Zeile nach unten" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = " Zeile nach oben" })
+
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = " Zeile nach unten" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = " Zeile nach oben" })
